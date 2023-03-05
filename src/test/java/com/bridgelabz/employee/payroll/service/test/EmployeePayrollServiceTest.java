8a47ba60d17e2874c958365baa2031f576593266
Prashant Deepake <deepakeprashant1@gmail.com>
2023-03-06 00:03:03 +0530

    Demonstrate File Operation

---
+++ src/test/java/com/bridgelabz/employee/payroll/service/test/EmployeePayrollServiceTest.java
@@ -0,0 +1,48 @@
+package com.bridgelabz.employee.payroll.service.test;
+
+import com.bridgelabz.employee.payroll.service.FileUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.IntStream;
+
+public class EmployeePayrollServiceTest {
+    private static final String HOME = System.getProperty("user.home");
+    private static final String PLAY_WITH_NIO = "PlayGround";
+
+    @Test
+    void givenPathName_WhenChecked_ThenConfirm() throws IOException {
+
+        Path homePath = Paths.get(HOME);
+        Assertions.assertTrue(Files.exists(homePath));
+
+        Path playPath = Paths.get(HOME + "/" + PLAY_WITH_NIO);
+        if (Files.exists(playPath))
+            FileUtils.deleteFiles(playPath.toFile());
+        Assertions.assertTrue(Files.notExists(playPath));
+
+        Files.createDirectory(playPath);
+        Assertions.assertTrue(Files.exists(playPath));
+
+        IntStream.range(1, 10).forEach(cntr -> {
+            Path tempFile = Paths.get(playPath + "/temp" + cntr);
+            Assertions.assertTrue(Files.notExists(tempFile));
+            try {
+                Files.createFile(tempFile);
+            } catch (IOException e) {
+                Assertions.assertTrue(Files.exists(tempFile));
+            }
+        });
+
+        Files.list(playPath).filter(Files::isRegularFile).forEach(System.out::println);
+        Files.newDirectoryStream(playPath).forEach(System.out::println);
+        Files.newDirectoryStream(playPath, path -> path.toFile().isFile() &&
+                        path.toString().startsWith("temp"))
+                .forEach(System.out::println);
+    }
+
+}
